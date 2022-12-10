bdc2032aa28d1b1a2f1c574f3b41810846d44bd1
Elmeri Lehikoinen <elmeri.lehikoinen@gmail.com>
2022-12-10 18:57:31 +0200

    Added sounds to assets folder

---
+++ Assets/Code/Sound/SoundHandler.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundHandler : MonoBehaviour
+{
+
+    public GroundedCharacterController playerMovementScript;
+
+    public AudioSource jumpSound;
+    public AudioSource footstep_1;
+    public AudioSource footstep_2;
+    public AudioSource shield_block;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
